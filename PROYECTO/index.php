5f74324da95a5357d353e3bcb8adb687645fc016
GiovanniHC1998 <42759358+GiovanniHC1998@users.noreply.github.com>
2018-11-11 17:28:07 -0500

    Update index.php

--- PROYECTO/index.php
+++ PROYECTO/index.php
@@ -3,5 +3,6 @@
 <html>
 <a href="DESIGNER/frmInstituciones.php">Instituciones</a><br>
 <a href="DESIGNER/frmestudiante.php">Estudiante</a><br>
+<a href="DESIGNER/frmProceso_Docentes.php">Docente</a><br>  
 
 </html>
