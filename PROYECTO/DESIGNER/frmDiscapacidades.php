376b3a11890accaff10fd9ee51df7c9ff639b227
MARIFLOR21 <44596077+MARIFLOR21@users.noreply.github.com>
2018-11-12 12:38:29 -0500

    Actualización de Html - Discapacidades

---
+++ PROYECTO/DESIGNER/frmDiscapacidades.php
@@ -0,0 +1,99 @@
+<?php
+	require_once('../BOL/discapacidades.php');
+	require_once('../DAO/discapacidadesDAO.php');
+
+	$dis = new Discapacidades();
+	$disDAO = new DiscapacidadesDAO();
+
+
+	if(isset($_POST['guardar']))
+	{
+		$dis->__SET('Descripcion',         		 $_POST['des']);
+
+		$disDAO->Registrar($dis);
+		header('Location: frmDiscapacidades.php');
+	}
+
+	if(isset($_POST['regresar']))
+	{
+	header('Location: ../procesos_registroEstudiantes.php');
+	}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Discapacidades</title>
+        <link rel="stylesheet" type="text/css" href="css/pure-min.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	</head>
+    <body style="padding:15px;">
+
+        <div class="pure-g">
+            <div class="pure-u-1-12">
+
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post" class="pure-form pure-form-stacked" style="margin-bottom:30px;">
+
+                    <table style="width:500px;" border="0">
+                    	<!-- esto es un comentario en Html
+						Con respecto a estas lineas se ha implementado para ingresar el codi
+						go del curso
+                    	-->
+                        <tr>
+                            <th style="text-align:left;">Descripcion:</th>
+                            <td><input type="text" name="des" value="" style="width:100%;" /></td>
+                        </tr>
+ 
+                        <tr>
+                            <td colspan="2">
+									<input type="submit" value="GUARDAR" name="guardar"class="pure-button pure-button-primary">
+									<input type="submit" value="BUSCAR" name="buscar"class="pure-button pure-button-primary">
+									<input type="submit" value="REGRESAR" name="regresar"class="pure-button pure-button-primary">
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+
+
+            </div>
+        </div>
+
+				<!--ESTA CONDICION SIRVE PARA REALIZAR BUSQUEDA POR DNI-->
+
+				<?php
+				if(isset($_POST['buscar']))
+				{
+					$resultado = array();//VARIABLE TIPO RESULTADO
+					$dis->__SET('Descripcion',          $_POST['des']);//ESTABLECEMOS EL VALOR DEL DNI
+					$resultado = $disDAO->Listar($dis); //CARGAMOS LOS REGISTRO EN EL ARRAY RESULTADO
+					if(!empty($resultado)) //PREGUNTAMOS SI NO ESTA VACIO EL ARRAY
+					{
+						?>
+						<table class="pure-table pure-table-horizontal">
+								<thead>
+										<tr>
+												<th style="text-align:left;">ID</th>
+												<th style="text-align:left;">Discapacidades</th>
+										</tr>
+								</thead>
+						<?php
+						foreach( $resultado as $r): //RECORREMOS EL ARRAY RESULTADO A TRAVES DE SUS CAMPOS
+							?>
+								<tr>
+										<td><?php echo $r->__GET('Cod_Discapacidad'); ?></td>
+										<td><?php echo $r->__GET('Descripcion'); ?></td>
+								</tr>
+						<?php endforeach;
+					}
+					else
+					{
+						echo 'no se encuentra en la base de datos!';
+					}
+					?>
+					</table>
+					<?php
+				}
+				?>
+
+    </body>
+</html>
